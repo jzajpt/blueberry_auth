4fd9b47ffc3c19e530d086fb4ea53f5b25d86078
Jiri Zajpt <jzajpt@blueberry.cz>
2009-05-16 11:57:46 +0200

    Engine routes now loads before application's.

--- init.rb
+++ init.rb
@@ -1,2 +1,6 @@
-# Include hook code here
-I18n.load_path += Dir[Rails.root.join('vendor', 'plugins', 'blueberry_auth', 'config', 'locales', '*.yml')]  
+# Add locales
+# I18n.load_path += Dir[Rails.root.join('vendor', 'plugins', 'blueberry_auth', 'config', 'locales', '*.yml')]
+I18n.load_path << Dir[File.expand_path(File.dirname(__FILE__) + '/config/locales/*.yml')]
+
+require 'blueberry_auth'
+
--- lib/blueberry_auth.rb
+++ lib/blueberry_auth.rb
@@ -1,1 +1,13 @@
-# BlueberryAuth
+
+# Hack to load engine's routes after application routes, thank you Thoughtbot.
+class ActionController::Routing::RouteSet
+  def load_routes_with_evergreen!
+    evergreen_routes = File.expand_path(File.dirname(__FILE__) + '/../config/evergreen_routes.rb')
+    unless configuration_files.include? evergreen_routes
+      add_configuration_file(evergreen_routes)
+    end
+    load_routes_without_evergreen!
+  end
+
+  alias_method_chain :load_routes!, :evergreen
+end
